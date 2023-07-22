ef0021b7d05f86308df318e5d5325a9aea669331
ariunaamy <ariun.myagmar@gmail.com>
2023-07-22 17:32:15 -0400

    landing page

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -1,5 +1,8 @@
 import Button from "./design-components/Button";
+import NavBar from "./NavBar";
 import Description from "./design-components/Description";
+
+
 
 export default function LandingPage() {
   const handleClick = () => {
@@ -7,15 +10,21 @@ export default function LandingPage() {
   };
   return (
     <div className="bg-gradient-to-r from-[#c4ff6c] to-[#2ae200] h-screen">
-      <div className="flex flex-col items-center justify-center h-screen">
-        <h1 className="text-7xl font-extrabold tracking-tight text-white hover:text-indigo-500 text-center">
+      <NavBar/>
+      <div className="flex items-center justify-center">
+        <div className="grid grid-cols-2 gap-4 p-4">
+        <h1 className="text-7xl font-extrabold tracking-tight text-white hover:text-indigo-500">
           Tip Tap Tracker
-        </h1>
-        <Description/>
-        <div className="mt-12"> 
+        </h1> 
+        <img 
+        className="w-64 z-[50]"
+        src="src/assets/DALL·E_toddler.png" alt="banner"/>    
+        </div>  
+      </div>
+      <Description/>
+        <div className="mt-12 text-center"> 
           <Button onClick={handleClick}>START</Button>
         </div>
-      </div>
     </div>
   );
 }
