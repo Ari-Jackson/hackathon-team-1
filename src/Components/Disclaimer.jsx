0589111c449f0fe0c340bdae6345e16badbdf0cc
ariunaamy <ariun.myagmar@gmail.com>
2023-07-24 11:39:02 -0400

    added

--- src/Components/Disclaimer.jsx
+++ src/Components/Disclaimer.jsx
@@ -10,19 +10,19 @@ export default function Disclaimer() {
 
   return (
     <div className="h-screen">
-        <div className="mx-auto mt-[10%] rounded-lg shadow-md bg-white w-[30%] h-[60%] flex grid  p-12">
-        <p className="text-3xl font-extrabold tracking-tight text-[#053560] mt-1">
-        <div className="text-orange-400 p-0 m-0">&#9888;&#65039;</div>
-        <br/>
-        This is a preliminary assessment tool.
-        <br />
-        This application is not designed to make any formal diagnosis.
-        <br /> Any diagnosis must be made by a medical professional.
-      </p>
-
-        </div>
-        <div className="flex justify-center mt-32 m-24 ">
-      <button
+      <div className="mx-auto mt-[10%] rounded-lg shadow-md bg-white w-[30%] h-[65%] flex grid  p-12">
+        <p className=" text-3xl font-extrabold tracking-tight text-[#053560]">
+          <div className="text-orange-400 p-0 m-0">&#9888;&#65039;</div>
+          <br />
+          This is a preliminary assessment tool.
+          <br />
+          This application is not designed to make any formal diagnosis.
+          <br /> Any diagnosis must be made by a medical professional.
+        </p>
+        <p className="font-semibold">You would need pencil and papaer ✏️📓</p>
+      </div>
+      <div className="flex justify-center mt-32 m-24 ">
+        <button
           onClick={handleClick}
           type="button"
           className="ml-inline-block rounded border-2 border-[#0065ff] px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-primary transition duration-150 ease-in-out hover:border-purple-600 hover:text-purple-600 hover:bg-opacity-10 hover:text-primary-600 focus:border-primary-600 focus:text-primary-600 focus:outline-none focus:ring-0 active:border-primary-700 active:text-primary-700 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10 text-[#0065ff]"
@@ -30,7 +30,7 @@ export default function Disclaimer() {
         >
           Continue &gt;&gt;
         </button>
-        </div>
+      </div>
     </div>
   );
 }
