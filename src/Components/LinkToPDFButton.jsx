9ecfb4f99fdc8841f2a9a7fb7601ebb538ffdc78
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-07-24 10:58:42 -0400

    Styles PDF button

--- src/Components/LinkToPDFButton.jsx
+++ src/Components/LinkToPDFButton.jsx
@@ -1,16 +1,16 @@
+import { Link } from "react-router-dom";
+
 const LinkToPDFButton = () => {
   return (
-    <div>
-      <a
-        href="https://www.michaeltaymormd.com/storage/app/media/forms/master-set-asq.pdf"
+    <button className="py-4 text-xl text-center text-white transition-colors duration-300 bg-green-400 rounded-full hover:bg-green-500 ease px-9 md:w-auto">
+      <Link
+        to="https://www.michaeltaymormd.com/storage/app/media/forms/master-set-asq.pdf"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <button className="w-full py-4 text-xl text-center text-white transition-colors duration-300 bg-green-400 rounded-full hover:bg-green-500 ease px-9 md:w-auto">
-          Download PDF
-        </button>
-      </a>
-    </div>
+        Download ASQ PDF
+      </Link>
+    </button>
   );
 };
 
